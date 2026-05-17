3993dc118adc32a8f5428050e29d270a57d158a2
Shefa Hathoon <it254103725@my.sliit.lk>
2026-05-17 23:21:53 +0100

    Added the Admin.java file

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Admin {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
